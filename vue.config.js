a53ffc909cc865d03a0e555620c553882a1e8438
Woozyzzz <woozyzzz@foxmail.com>
2023-08-16 14:37:21 +0800

    chore: 修改页面标题

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "PPT",
+    },
+  },
 });
